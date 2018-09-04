14caff2d836dec7080fc00c8a71d819654933a83
mariamjoll1 <mariahrafns@gmail.com>
2018-09-04 13:45:16 +0000

    uppfært

--- max_int.py
+++ max_int.py
@@ -1,17 +1,11 @@
+#ég vil taka öll input og athuga hvað þeirra er hæðst, þangað til notandi slær inn mínus tölu, 
+# þá á algarithmið að hætta og prenta út hæðstu töluna
 
 num_int = int(input("Input a number: "))    # Do not change this line
 max_int = num_int
 
-while num_int != 0:
+while num_int > 0:
     num_int = int(input("Input a number: "))
-    max_int = num_int
-    if num_int > 0:
-
-        for max_int in num_int:
-            if max_int > 0:
-                max_int = max(num_int)
-                print("The maximum is: ", max_int)
-            else:
-                print("error")
-
-#print("The maximum is", max_int)    # Do not change this line
+    if num_int > max_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
